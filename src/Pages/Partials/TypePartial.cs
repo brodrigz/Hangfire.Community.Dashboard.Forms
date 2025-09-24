8ae2c313c22a11138f93c4a2791ce93ae25f205b
Bruno Rodrigues <Bruno.Rodrigues@flex.com>
2025-09-24 17:54:09 -0300

    fix html not being returned on single interface impl

--- src/Pages/Partials/TypePartial.cs
+++ src/Pages/Partials/TypePartial.cs
@@ -11,143 +11,144 @@ using Hangfire.Community.Dashboard.Forms.Support;
 
 namespace Hangfire.Community.Dashboard.Forms.Partials
 {
-    public static class TypePartial
-    {
-        public static string ToHtml(Type type, string id, DisplayDataAttribute displayInfo, int listDepth, object defaultValue = null, HashSet<Type> nAllowedTypes = null)
-        {
-            if (type == null) throw new ArgumentNullException(nameof(type));
-            if (id == null) throw new ArgumentNullException(nameof(id));
-            if (displayInfo == null) throw new ArgumentNullException(nameof(displayInfo));
+	public static class TypePartial
+	{
+		public static string ToHtml(Type type, string id, DisplayDataAttribute displayInfo, int listDepth, object defaultValue = null, HashSet<Type> nAllowedTypes = null)
+		{
+			if (type == null) throw new ArgumentNullException(nameof(type));
+			if (id == null) throw new ArgumentNullException(nameof(id));
+			if (displayInfo == null) throw new ArgumentNullException(nameof(displayInfo));
 			if (nAllowedTypes == null) nAllowedTypes = new HashSet<Type>();
 
 			bool isGeneric = type.IsGenericType;
-            bool isList = isGeneric && type.GetGenericTypeDefinition() == typeof(List<>);
-            bool isNullable = isGeneric && type.GetGenericTypeDefinition() == typeof(Nullable<>);
-            bool isLoaded = defaultValue != null;
+			bool isList = isGeneric && type.GetGenericTypeDefinition() == typeof(List<>);
+			bool isNullable = isGeneric && type.GetGenericTypeDefinition() == typeof(Nullable<>);
+			bool isLoaded = defaultValue != null;
 
-            string inputTMP = string.Empty;
-            Type genericArgument = type.IsGenericType ? type.GetGenericArguments()[0] : null; //multiple generic arguments are not supported
+			string inputTMP = string.Empty;
+			Type genericArgument = type.IsGenericType ? type.GetGenericArguments()[0] : null; //multiple generic arguments are not supported
 
-            string labelText = displayInfo.Label ?? genericArgument?.Name ?? type.Name;
-            string placeholderText = displayInfo.Placeholder ?? labelText;
+			string labelText = displayInfo.Label ?? genericArgument?.Name ?? type.Name;
+			string placeholderText = displayInfo.Placeholder ?? labelText;
 
-            if (type == typeof(string))
-            {
-                return FormPartial.InputString(id, displayInfo.CssClasses, labelText, placeholderText, displayInfo.Description, defaultValue, displayInfo.IsDisabled, displayInfo.IsRequired, displayInfo.IsMultiLine);
-            }
-            else if (type == typeof(int) || type == typeof(int?))
-            {
-                return FormPartial.InputInteger(id, displayInfo.CssClasses, labelText, placeholderText, displayInfo.Description, defaultValue, displayInfo.IsDisabled, displayInfo.IsRequired);
-            }
-            else if (type == typeof(Uri))
-            {
-                return FormPartial.Input(id, displayInfo.CssClasses, labelText, placeholderText, displayInfo.Description, "url", defaultValue, displayInfo.IsDisabled, displayInfo.IsRequired);
-            }
-            else if (type == typeof(DateTime) || type == typeof(DateTime?))
-            {
-                return FormPartial.InputDateTime(id, displayInfo.CssClasses, labelText, placeholderText, displayInfo.Description, defaultValue, displayInfo.IsDisabled, displayInfo.IsRequired, displayInfo.ControlConfiguration);
-            }
-            else if (type == typeof(bool))
-            {
-                return FormPartial.InputBoolean(id, displayInfo.CssClasses, labelText, placeholderText, displayInfo.Description, defaultValue, displayInfo.IsDisabled);
-            }
-            else if (type.IsEnum || (isNullable && genericArgument.IsEnum))
-            {
-                var data = new Dictionary<string, int>();
-                foreach (var name in Enum.GetNames(type.IsEnum ? type : genericArgument))
-                {
-                    var value = (int)Enum.Parse(type.IsEnum ? type : genericArgument, name);
-                    data.Add(name, value);
-                }
+			if (type == typeof(string))
+			{
+				return FormPartial.InputString(id, displayInfo.CssClasses, labelText, placeholderText, displayInfo.Description, defaultValue, displayInfo.IsDisabled, displayInfo.IsRequired, displayInfo.IsMultiLine);
+			}
+			else if (type == typeof(int) || type == typeof(int?))
+			{
+				return FormPartial.InputInteger(id, displayInfo.CssClasses, labelText, placeholderText, displayInfo.Description, defaultValue, displayInfo.IsDisabled, displayInfo.IsRequired);
+			}
+			else if (type == typeof(Uri))
+			{
+				return FormPartial.Input(id, displayInfo.CssClasses, labelText, placeholderText, displayInfo.Description, "url", defaultValue, displayInfo.IsDisabled, displayInfo.IsRequired);
+			}
+			else if (type == typeof(DateTime) || type == typeof(DateTime?))
+			{
+				return FormPartial.InputDateTime(id, displayInfo.CssClasses, labelText, placeholderText, displayInfo.Description, defaultValue, displayInfo.IsDisabled, displayInfo.IsRequired, displayInfo.ControlConfiguration);
+			}
+			else if (type == typeof(bool))
+			{
+				return FormPartial.InputBoolean(id, displayInfo.CssClasses, labelText, placeholderText, displayInfo.Description, defaultValue, displayInfo.IsDisabled);
+			}
+			else if (type.IsEnum || (isNullable && genericArgument.IsEnum))
+			{
+				var data = new Dictionary<string, int>();
+				foreach (var name in Enum.GetNames(type.IsEnum ? type : genericArgument))
+				{
+					var value = (int)Enum.Parse(type.IsEnum ? type : genericArgument, name);
+					data.Add(name, value);
+				}
 
-                return FormPartial.InputEnum(id, displayInfo.CssClasses, labelText, placeholderText, displayInfo.Description, data, defaultValue?.ToString(), displayInfo.IsDisabled);
-            }
+				return FormPartial.InputEnum(id, displayInfo.CssClasses, labelText, placeholderText, displayInfo.Description, data, defaultValue?.ToString(), displayInfo.IsDisabled);
+			}
 
-            if (type.IsClass && !isGeneric)
-            {
-                if (!nAllowedTypes.Add(type)) { return "<span>Circular reference detected, not allowed.</span>"; } //Circular reference, not allowed -> null
+			if (type.IsClass && !isGeneric)
+			{
+				if (!nAllowedTypes.Add(type)) { return "<span>Circular reference detected, not allowed.</span>"; } //Circular reference, not allowed -> null
 
-                inputTMP += $"<div class=\"panel panel-default\"><div class=\"panel-heading\" role=\"button\" data-toggle=\"collapse\" href=\"#collapse_{id}\" aria-expanded=\"false\" aria-controls=\"collapse_{id}\"><h4 class=\"panel-title\">{labelText}</h4></div><div id=\"collapse_{id}\" class=\"panel-collapse collapse\"><div class=\"panel-body\">";
+				inputTMP += $"<div class=\"panel panel-default\"><div class=\"panel-heading\" role=\"button\" data-toggle=\"collapse\" href=\"#collapse_{id}\" aria-expanded=\"false\" aria-controls=\"collapse_{id}\"><h4 class=\"panel-title\">{labelText}</h4></div><div id=\"collapse_{id}\" class=\"panel-collapse collapse\"><div class=\"panel-body\">";
 
-                foreach (var propertyInfo in type.GetProperties().Where(prop => Attribute.IsDefined(prop, typeof(DisplayDataAttribute))))
-                {
-                    var propDisplayInfo = propertyInfo.GetCustomAttribute<DisplayDataAttribute>();
+				foreach (var propertyInfo in type.GetProperties().Where(prop => Attribute.IsDefined(prop, typeof(DisplayDataAttribute))))
+				{
+					var propDisplayInfo = propertyInfo.GetCustomAttribute<DisplayDataAttribute>();
 					propDisplayInfo.Label = propDisplayInfo.Label ?? propertyInfo.Name;
 					var propDefaultValue = isLoaded ? defaultValue?.GetType().GetProperty(propertyInfo.Name)?.GetValue(defaultValue) : propDisplayInfo.DefaultValue;
-                    string propId = $"{id}_{propertyInfo.Name}";
+					string propId = $"{id}_{propertyInfo.Name}";
 
-                    inputTMP += ToHtml(propertyInfo.PropertyType, propId, propDisplayInfo, listDepth, propDefaultValue, nAllowedTypes);
-                }
+					inputTMP += ToHtml(propertyInfo.PropertyType, propId, propDisplayInfo, listDepth, propDefaultValue, nAllowedTypes);
+				}
 
 				nAllowedTypes.Remove(type);
 
-                return inputTMP += "</div></div></div>";
-            }
+				return inputTMP += "</div></div></div>";
+			}
 
-            if (type.IsInterface)
-            {
-                if (!VT.Implementations.ContainsKey(type)) { return $"<span>No concrete implementation of \"{type.Name}\" found in the current assembly.</span>"; }
+			if (type.IsInterface)
+			{
+				if (!VT.Implementations.ContainsKey(type)) { return $"<span>No concrete implementation of \"{type.Name}\" found in the current assembly.</span>"; }
 
-                var impls = VT.Implementations[type];
+				var impls = VT.Implementations[type];
 
-                if (impls == null || impls.Count < 1)
-                {
-                    return $"<span>No concrete implementation of \"{type.Name}\" found in the current assembly.</span>";
-                }
+				if (impls == null || impls.Count < 1)
+				{
+					return $"<span>No concrete implementation of \"{type.Name}\" found in the current assembly.</span>";
+				}
 
-                if (impls.Count == 1)
-                {
-                    var implType = impls.First();
+				if (impls.Count == 1)
+				{
+					var implType = impls.First();
 					inputTMP += $"<div class=\"panel panel-default\"><div class=\"panel-heading\" role=\"button\" data-toggle=\"collapse\" href=\"#collapse_{id}_{type.Name}\" aria-expanded=\"false\" 	aria-controls=\"collapse_{id}_{type.Name}\"><h4 class=\"panel-title\">{type.Name}</h4></div><div id=\"collapse_{id}_{type.Name}\" class=\"panel-collapse collapse\"><div class=\"panel-body\">";
-                    inputTMP += ToHtml(implType, $"{id}_{implType.Name}", displayInfo, listDepth, defaultValue, nAllowedTypes);
-                    inputTMP += "</div></div></div>";
-                }
-                else
-                {
-                    var filteredImpls = new HashSet<Type>(impls.Where(impl => !nAllowedTypes.Contains(impl)));
+					inputTMP += ToHtml(implType, $"{id}_{implType.Name}", displayInfo, listDepth, defaultValue, nAllowedTypes);
+					inputTMP += "</div></div></div>";
+					return inputTMP;
+				}
+				else
+				{
+					var filteredImpls = new HashSet<Type>(impls.Where(impl => !nAllowedTypes.Contains(impl)));
 
 					//currently default value for interface is not supported.
 					Type defaultImplType = isLoaded ? defaultValue.GetType() : (Type)defaultValue ?? null;
 
 					if (defaultImplType != null)
-                    {
-                        if (!type.IsAssignableFrom(defaultImplType)) { return $"<span>Default type \"{defaultImplType.Name}\" does not implement interface \"{type.Name}\".</span>"; }
-                        if (!impls.Contains(defaultImplType)) { return $"<span>Default type \"{defaultImplType.Name}\" is not in the list of implementations.</span>"; }
-                        if (!filteredImpls.Contains(defaultImplType)) { return $"<span>Default type \"{defaultImplType.Name}\" creates a circular reference and is not allowed.</span>"; }
-                    }
+					{
+						if (!type.IsAssignableFrom(defaultImplType)) { return $"<span>Default type \"{defaultImplType.Name}\" does not implement interface \"{type.Name}\".</span>"; }
+						if (!impls.Contains(defaultImplType)) { return $"<span>Default type \"{defaultImplType.Name}\" is not in the list of implementations.</span>"; }
+						if (!filteredImpls.Contains(defaultImplType)) { return $"<span>Default type \"{defaultImplType.Name}\" creates a circular reference and is not allowed.</span>"; }
+					}
 
-                    inputTMP += FormPartial.InputImplsMenu(id, displayInfo.CssClasses, labelText, displayInfo.Placeholder, displayInfo.Description, filteredImpls, defaultImplType, displayInfo.IsDisabled, displayInfo.IsRequired);
+					inputTMP += FormPartial.InputImplsMenu(id, displayInfo.CssClasses, labelText, displayInfo.Placeholder, displayInfo.Description, filteredImpls, defaultImplType, displayInfo.IsDisabled, displayInfo.IsRequired);
 
 					//Concrete
-                    foreach (Type impl in filteredImpls)
-                    {
-                        if (!nAllowedTypes.Add(impl)) { return "<span>Circular reference detected, not allowed.</span>"; } //Circular reference, not allowed -> null
+					foreach (Type impl in filteredImpls)
+					{
+						if (!nAllowedTypes.Add(impl)) { return "<span>Circular reference detected, not allowed.</span>"; } //Circular reference, not allowed -> null
 
-                        var dNone = impl.IsEquivalentTo(defaultImplType) ? "" : "d-none";
+						var dNone = impl.IsEquivalentTo(defaultImplType) ? "" : "d-none";
 
-                        inputTMP += $"<div id=\"{id}_{impl.Name}\" class=\"panel panel-default impl-panels-for-{id} {dNone}\"><div class=\"panel-heading\" role=\"button\" data-toggle=\"collapse\" href=\"#collapse_{id}_{impl.Name}\" aria-expanded=\"false\" aria-controls=\"collapse_{id}_{impl.Name}\"><h4 class=\"panel-title\">{impl.Name} | {type.Name}</h4></div><div id=\"collapse_{id}_{impl.Name}\" 	class=\"panel-collapse collapse\"><div 	class=\"panel-body\">";
+						inputTMP += $"<div id=\"{id}_{impl.Name}\" class=\"panel panel-default impl-panels-for-{id} {dNone}\"><div class=\"panel-heading\" role=\"button\" data-toggle=\"collapse\" href=\"#collapse_{id}_{impl.Name}\" aria-expanded=\"false\" aria-controls=\"collapse_{id}_{impl.Name}\"><h4 class=\"panel-title\">{impl.Name} | {type.Name}</h4></div><div id=\"collapse_{id}_{impl.Name}\" 	class=\"panel-collapse collapse\"><div 	class=\"panel-body\">";
 
-                        foreach (var propertyInfo in impl.GetProperties().Where(prop => Attribute.IsDefined(prop, typeof(DisplayDataAttribute))))
-                        {
-                            var propDisplayInfo = propertyInfo.GetCustomAttribute<DisplayDataAttribute>();
-				        	propDisplayInfo.Label = propDisplayInfo.Label ?? propertyInfo.Name;
-				        	var propDefaultValue = isLoaded ? defaultValue?.GetType().GetProperty(propertyInfo.Name)?.GetValue(defaultValue) : propDisplayInfo.DefaultValue;
-                            string propId = $"{id}_{impl.Name}_{propertyInfo.Name}";
+						foreach (var propertyInfo in impl.GetProperties().Where(prop => Attribute.IsDefined(prop, typeof(DisplayDataAttribute))))
+						{
+							var propDisplayInfo = propertyInfo.GetCustomAttribute<DisplayDataAttribute>();
+							propDisplayInfo.Label = propDisplayInfo.Label ?? propertyInfo.Name;
+							var propDefaultValue = isLoaded ? defaultValue?.GetType().GetProperty(propertyInfo.Name)?.GetValue(defaultValue) : propDisplayInfo.DefaultValue;
+							string propId = $"{id}_{impl.Name}_{propertyInfo.Name}";
 
-                            inputTMP += ToHtml(propertyInfo.PropertyType, propId, propDisplayInfo, listDepth, propDefaultValue, nAllowedTypes);
-                        }
+							inputTMP += ToHtml(propertyInfo.PropertyType, propId, propDisplayInfo, listDepth, propDefaultValue, nAllowedTypes);
+						}
 
-				        nAllowedTypes.Remove(impl);
+						nAllowedTypes.Remove(impl);
 
-                        inputTMP += "</div></div></div>";
-                    }
+						inputTMP += "</div></div></div>";
+					}
 
-                    return inputTMP;
-                }
-            }
+					return inputTMP;
+				}
+			}
 
-            if (isList)
-            {
+			if (isList)
+			{
 				//List<List<...<Concrete>>
 
 				// - List Wrapper
@@ -158,37 +159,37 @@ namespace Hangfire.Community.Dashboard.Forms.Partials
 				// --- Element Content
 
 				if (!isLoaded)
-                {
+				{
 
 					labelText += " | Collection of ";
 					var innerType = genericArgument;
 
 					// Append "Collection of " to labelText for each nested generic List<> or Nullable<>
 					while (innerType.IsGenericType)
-                    {
-                        if (innerType.GetGenericTypeDefinition() == typeof(List<>))
-                        {
-                            innerType = innerType.GetGenericArguments()[0];
-                            labelText += $"Collections of ";
-                        }
-                        else if (innerType.GetGenericTypeDefinition() == typeof(Nullable<>))
-                        {
-                            innerType = innerType.GetGenericArguments()[0];
-                        }
-                    }
+					{
+						if (innerType.GetGenericTypeDefinition() == typeof(List<>))
+						{
+							innerType = innerType.GetGenericArguments()[0];
+							labelText += $"Collections of ";
+						}
+						else if (innerType.GetGenericTypeDefinition() == typeof(Nullable<>))
+						{
+							innerType = innerType.GetGenericArguments()[0];
+						}
+					}
 
 					labelText += $"{innerType.Name}";
 					displayInfo.Label = $"{innerType.Name} | Element";
 
 					//if we are not loading a job, we can Depth-first visiting till we find a concrete type, then we process it as d-none element (FE will use it as template)
-                    return FormPartial.InputList(id, labelText, 0,
-                        FormPartial.InputElementList(0, listDepth, "d-none",
-                            ToHtml(genericArgument, $"{id}_list_0", displayInfo, listDepth + 1, defaultValue)));
-                }
-                else
-                {
-                    //if we loading a job, Breadth-first visiting
-                    IList defaultValueList = (IList)defaultValue;
+					return FormPartial.InputList(id, labelText, 0,
+						FormPartial.InputElementList(0, listDepth, "d-none",
+							ToHtml(genericArgument, $"{id}_list_0", displayInfo, listDepth + 1, defaultValue)));
+				}
+				else
+				{
+					//if we loading a job, Breadth-first visiting
+					IList defaultValueList = (IList)defaultValue;
 
 					labelText += " | Collection of ";
 					var innerType = genericArgument;
@@ -211,15 +212,15 @@ namespace Hangfire.Community.Dashboard.Forms.Partials
 					displayInfo.Label = $"{innerType.Name} | Element";
 
 					for (int i = 0; i < defaultValueList.Count; i++)
-                    {
-                        var elementValue = defaultValueList[i];
+					{
+						var elementValue = defaultValueList[i];
 
-                        inputTMP += FormPartial.InputElementList(i, listDepth, "",
-                            ToHtml(genericArgument, $"{id}_list_{i}", displayInfo, listDepth + 1, elementValue));
-                    }
+						inputTMP += FormPartial.InputElementList(i, listDepth, "",
+							ToHtml(genericArgument, $"{id}_list_{i}", displayInfo, listDepth + 1, elementValue));
+					}
 
 					//the template atleast
-					if(defaultValueList.Count == 0)
+					if (defaultValueList.Count == 0)
 					{
 						return FormPartial.InputList(id, labelText, 0,
 							FormPartial.InputElementList(0, listDepth, "d-none",
@@ -227,11 +228,11 @@ namespace Hangfire.Community.Dashboard.Forms.Partials
 					}
 
 					return FormPartial.InputList(id, labelText, ((IList)defaultValue).Count, inputTMP);
-                }
+				}
 
-            }
-            
-            return inputTMP = "<span>Unsupported type or not implemented yet.</span>";
-        }
-    }   
+			}
+
+			return "<span>Unsupported type or not implemented yet.</span>";
+		}
+	}
 }
